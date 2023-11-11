70f86860dbd9b5dd344ef8eed5b2b8a9b4c6da3f
pnn1 <jiracheep.somnuek@gmail.com>
2023-11-12 01:51:31 +0700

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is a demo with Node.js and Express.js on Vercel!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
