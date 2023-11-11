99f4a47250023671b44e62344a5c0505d1854f71
pnn1 <jiracheep.somnuek@gmail.com>
2023-11-12 00:33:25 +0700

    start

--- express/server.js
+++ express/server.js
@@ -4,19 +4,33 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
 const router = express.Router();
+
+// Link to views folder.
+let views = path.join(__dirname, '../');
+
+// Home route.
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.sendFile('index.html', { root: views });
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// Other routes.
+router.get('/page1', function(req, res){
+  res.sendFile('page1.html', { root: views });
+});
+router.get('/page2', function(req, res){
+  res.sendFile('page2.html', { root: views });
+});
+router.get('/page3', function(req, res){
+  res.sendFile('page3.html', { root: views });
+});
+router.get('/page4', function(req, res){
+  res.sendFile('page4.html', { root: views });
+});
+
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/.netlify/functions/server', router);  // path must route to lambda (express/server.js)
 
 module.exports = app;
 module.exports.handler = serverless(app);
